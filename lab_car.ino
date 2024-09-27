f5f5b1c1372c1cd638237c003f73a5bb76c49331
Your Name <you@example.com>
2024-09-27 22:21:17 +0800

    upload an easy motor program

--- lab_car.ino
+++ lab_car.ino
@@ -1,48 +1,45 @@
-#include <AFMotor.h>
+// Adafruit Motor shield library
+// copyright Adafruit Industries LLC, 2009
+// this code is public domain, enjoy!
 
-AF_DCMotor motor(3);
-AF_DCMotor motor1(1);
-AF_DCMotor motor2(2);
-AF_DCMotor motor4(4);
+#include "AFMotor.h"
+
+AF_DCMotor motor(1);
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Motor test!");
 
+  // turn on motor
+  motor.setSpeed(200);
+  motor.run(RELEASE);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  motor.setSpeed(200);
-  motor1.setSpeed(200);
-  motor2.setSpeed(200);
-  motor4.setSpeed(200);
-
+  uint8_t i;
+  Serial.print("tick");
   motor.run(FORWARD);
-  motor1.run(FORWARD);
-  motor2.run(FORWARD);
-  motor4.run(FORWARD);
-
-  delay(2000);
-
-  motor.run(RELEASE);
-  motor1.run(RELEASE);
-  motor2.run(RELEASE);
-  motor4.run(RELEASE);
-
-  delay(1000);
-
+  for (i = 0; i < 255; i++) {
+    motor.setSpeed(i);  
+    delay(10);
+ }
+  for (i = 255; i != 0; i--) {
+    motor.setSpeed(i);  
+    delay(10);
+ }
+  
+  Serial.print("tock");
   motor.run(BACKWARD);
-  motor1.run(BACKWARD);
-  motor2.run(BACKWARD);
-  motor4.run(BACKWARD);
-
-  delay(2000);
-
+  for (i = 0; i < 255; i++) {
+    motor.setSpeed(i);  
+    delay(10);
+ }
+ 
+  for (i = 255; i != 0; i--) {
+    motor.setSpeed(i);  
+    delay(10);
+ }
+  Serial.print("tech");
   motor.run(RELEASE);
-  motor1.run(RELEASE);
-  motor2.run(RELEASE);
-  motor4.run(RELEASE);
-
   delay(1000);
-
 }
